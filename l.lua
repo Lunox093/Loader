054e53d82a48a95f4aeb208e89de13cd46c6fd79
Lunox093 <120286771+Lunox093@users.noreply.github.com>
2025-06-07 20:28:35 +0300

    Create l.lua

---
+++ l.lua
@@ -0,0 +1,15 @@
+if game.PlaceId == 120119241483509 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Lunox093/Pet-Incremental1/refs/heads/main/Script.lua"), true))()
+end
+
+if game.PlaceId == 101949297449238 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Lunox093/BuildAnIsland/refs/heads/main/Script"), true))()
+end
+
+if game.PlaceId == 76381016848158 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Lunox093/Factroy-RNG/refs/heads/main/Script"), true))()
+end
+
+if game.PlaceId == 3823781113 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Lunox093/test/refs/heads/main/script.lua"), true))()
+end
