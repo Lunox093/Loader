c22bf6c1c59ad3867ec3e8ab4424a417603e8b39
Lunox093 <120286771+Lunox093@users.noreply.github.com>
2026-04-10 21:20:16 +0300

    Update l.lua

--- l.lua
+++ l.lua
@@ -5,3 +5,7 @@ end
 if game.PlaceId == 3823781113 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Lunox093/test/refs/heads/main/script.lua"), true))()
 end
+
+if game.PlaceId == 3101667897 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Lunox093/speedlegends/refs/heads/main/.lua"), true))()
+end
